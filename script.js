b7895f928f4c17a8e0992faada353ca984a4ac81
capixm <capixm@gmail.com>
2021-02-23 13:58:29 +0000

    test slider

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    principal = document.getElementById("principal").value;
+    rate = document.getElementById("rate").value;
+    no_years = document.getElementById("years").years;
+
+    interest = principal * years * rate / 100;
+
+    current_year = new Date().getFullYear();
+
+    final_year = current_year + no_years;
+
+}
+
+function update_interest() {
+    current_interest = document.getElementById("rate").value;
+    document.getElementById("text_rate").innerHTML = current_interest;
 }
         
