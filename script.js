547a1a7d2f3ec06dda4da9563991a0777887b519
capixm <capixm@gmail.com>
2021-02-23 15:28:22 +0000

    bug fixed

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function compute()
 {
     principal = parseFloat(document.getElementById("principal").value);
-    window.alert("Principal equals to "+principal);
+    
     /*although the "principal" texbox is built to prevent user from entering
       negative numbers, an additional control is added just in case the user
       types the number directly*/
@@ -12,26 +12,21 @@ function compute()
     }
 
     rate = parseFloat(document.getElementById("rate").value);
-    window.alert("Rate is "+rate);
+    
 
     var box_element = document.getElementById("years");
     no_years = parseInt(box_element.options[box_element.selectedIndex].value);
-    window.alert("No. years is "+no_years);
-
+    
     interest = principal * no_years * rate / 100;
-    window.alert("Interest amounts to "+interest);
-
+    
     /*Date::getFullYear() returns current year in yyyy format*/
     var d = new Date();
     current_year = parseInt(d.getFullYear()); 
-    window.alert("current year is "+current_year);
-
+    
     final_year = current_year + no_years;
-    window.alert("final year is "+final_year);
-
+    
     total_amount = principal + interest;
-    window.alert("Total Amount is "+total_amount);
-
+    
     document.getElementById("result").innerHTML = 
     "<br/>If you deposit "+principal+" at a interest rate of "+rate+"%, "+
     "you will receive an amount of "+total_amount+" in the year "+final_year;
